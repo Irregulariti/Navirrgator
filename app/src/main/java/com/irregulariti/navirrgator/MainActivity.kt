79ae5c741bb8671682fd61c8a4ed3c4b9203f075
user <ermolin.andrei2017@yandex.ru>
2023-04-28 11:55:26 +0300

    added simple graphics for 1 stage & findTheWay 1 version

--- app/src/main/java/com/irregulariti/navirrgator/MainActivity.kt
+++ app/src/main/java/com/irregulariti/navirrgator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.irregulariti.navirrgator
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,20 +11,51 @@ import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.provider.Settings
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import com.irregulariti.navirrgator.ui.theme.NavirrgatorTheme
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Suppress("DEPRECATION")
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MutableCollectionMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            println(findTheWay(findTheNearest(listOf()), "510"))
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
+            var ok = false
+            var stage by remember { mutableStateOf(0) }
+            var point by remember { mutableStateOf("")}
+            var way by remember {mutableStateOf(mutableListOf<String>())}
+
             val wifiManager: WifiManager = getSystemService(WIFI_SERVICE) as WifiManager
             fun scanSuccess() {
                 val results = if (ActivityCompat.checkSelfPermission(
@@ -35,12 +67,10 @@ class MainActivity : ComponentActivity() {
                 } else {
                     listOf()
                 }
-                findTheNearest(results)
+                stage = returnStage(findTheNearest(results)).also{ point = findTheNearest(results)} // current stage
             }
 
-            fun scanFailure() {
-                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(myIntent)
+            fun scanFailure() { // permission menu
                 scanSuccess()
             }
 
@@ -66,6 +96,79 @@ class MainActivity : ComponentActivity() {
             val intentFilter = IntentFilter()
             intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
             registerReceiver(wifiScanReceiver, intentFilter)
+
+            if (stage == 1){
+                val firstColumn = mapOf("100а" to 1f, "лестница" to 1f, "?1" to 1f, "кафе лесное" to 2f, "?2" to 1f, "104" to 1f, "выход" to 3f, "106" to 1f, "108" to 1f, "?3" to 1f, "110" to 1f, "110а" to 1f, "лестница" to 1f, "3" to 1f)
+                val secondColumn = mapOf("" to 13f, "chill" to 1f)
+                val thirdColumn = mapOf("100.1" to 1f, "женский" to 1f, "администрация" to 1f, "?1" to 0.5f, "101" to 1f, "?2" to 0.5f, "102" to 1f, "103" to 1f, "лифты 1 этаж" to 3f, "105" to 1f, "?3" to 1f, "107" to 1f, "111" to 1f, "?4" to 1f, "мужской" to 1f, "109" to 1f)
+                println(way)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .align(Alignment.TopCenter)
+                            .padding(top = 5.dp),
+                        painter = painterResource(id = R.drawable.front_name),
+                        contentDescription = null
+                    )
+                }
+                Row(
+                    modifier = Modifier.padding(top = 100.dp),
+                    Arrangement.Center
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize().weight(1f)
+                    ) {
+                        for (i in 0..12) {
+                            val color = if (firstColumn.keys.toList()[i] !in way) { ButtonDefaults.buttonColors(backgroundColor = Color(0xA9FDFCFD))} else {ButtonDefaults.buttonColors(backgroundColor = Color(
+                                0xA9FC00FC
+                            )
+                            )}
+                            Button(
+                                modifier = Modifier.weight(firstColumn.values.toList()[i]).fillMaxSize(),
+                                onClick = {way = findTheWay(point, firstColumn.keys.toList()[i])},
+                                colors = color,
+                                border = BorderStroke(2.dp, Color(red = 0f, green = 0f, blue = 0f))
+                            ) {
+                                Text(firstColumn.keys.toList()[i])
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize().weight(1f)
+                    ) {
+                        for (i in 0..1) {
+                            Box(
+                                modifier = Modifier.weight(secondColumn.values.toList()[i]).fillMaxSize(),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Text(secondColumn.keys.toList()[i])
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize().weight(1f)
+                    ) {
+                        for (i in 0..15) {
+                            val color = if (thirdColumn.keys.toList()[i] !in way) { ButtonDefaults.buttonColors(backgroundColor = Color(0xA9FDFCFD))} else {ButtonDefaults.buttonColors(backgroundColor = Color(
+                                0xA9FC00FC
+                            )
+                            )}
+                            Button(
+                                modifier = Modifier.weight(thirdColumn.values.toList()[i]).fillMaxSize(),
+                                onClick = {way = findTheWay(point, firstColumn.keys.toList()[i])},
+                                colors = color,
+                                border = BorderStroke(2.7.dp, Color(red = 0f, green = 0f, blue = 0f))
+                            ) {
+                                Text(thirdColumn.keys.toList()[i],  modifier = Modifier, textAlign = TextAlign.Center)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -155,34 +258,22 @@ fun findTheWay(from: String, to: String): MutableList<String> {
 }
 
 fun diffStages(from: String, to: String): Int {
-    val fromInd = Valuable().graph.keys.indexOf(from)
-    var fromStage = 0
-    var toStage = 0
-    val toInd = Valuable().graph.keys.indexOf(to)
-    if (fromInd <= 20) {
-        fromStage = 1
-    } else if (fromInd in 22..43) {
-        fromStage = 2
-    } else if (fromInd in 45..60) {
-        fromStage = 3
-    } else if (fromInd in 62..75) {
-        fromStage = 4
-    } else if (fromInd in 77..90) {
-        fromStage = 5
-    }
-
-    if (toInd <= 20) {
-        toStage = 1
-    } else if (toInd in 22..43) {
-        toStage = 2
-    } else if (toInd in 45..60) {
-        toStage = 3
-    } else if (toInd in 62..75) {
-        toStage = 4
-    } else if (toInd in 77..90) {
-        toStage = 5
-    }
-    return kotlin.math.abs(fromStage - toStage)
+    return kotlin.math.abs(returnStage(from) - returnStage(to))
 }
 
-
+fun returnStage(point: String): Int{
+    var stage = 0
+    val ind = Valuable().graph.keys.indexOf(point)
+    if (ind <= 20) {
+        stage = 1
+    } else if (ind in 22..43) {
+        stage = 2
+    } else if (ind in 45..60) {
+        stage = 3
+    } else if (ind in 62..75) {
+        stage = 4
+    } else if (ind in 77..90) {
+        stage = 5
+    }
+    return stage
+}
